5c4bb3d62a32c6dee3602a1d7105c7422e98b2d8
Ricardo Gomes <ricky94slb@gmail.com>
2020-02-12 10:26:18 +0000

    Wishlist - Part 1

---
+++ src/Infrastructure/Data/Config/WishlistItemConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.eShopWeb.ApplicationCore.Entities.WishlistAggregate;
+
+namespace Microsoft.eShopWeb.Infrastructure.Data.Config
+{
+    public class WishlistItemConfiguration : IEntityTypeConfiguration<WishlistItem>
+    {
+        public void Configure(EntityTypeBuilder<WishlistItem> builder)
+        {
+            builder.Property(bi => bi.UnitPrice)
+                .IsRequired(true)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
