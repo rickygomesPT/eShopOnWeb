33796328ac4ee17ba64af87f03a546d933bc39f3
Ricardo Gomes <ricky94slb@gmail.com>
2020-02-13 11:50:48 +0000

    Remove Catalog Item

---
+++ src/Web/Pages/Admin/RemoveCatalogItem.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.eShopWeb.ApplicationCore.Constants;
+using Microsoft.eShopWeb.Web.Interfaces;
+using Microsoft.eShopWeb.Web.ViewModels;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.Web.Pages.Admin
+{
+    [Authorize(Roles = AuthorizationConstants.Roles.ADMINISTRATORS)]
+    public class RemoveCatalogItemModel : PageModel
+    {
+        private readonly ICatalogItemViewModelService _catalogItemViewModelService;
+
+        public RemoveCatalogItemModel(ICatalogItemViewModelService catalogItemViewModelService)
+        {
+            _catalogItemViewModelService = catalogItemViewModelService;
+        }
+
+        [BindProperty]
+        public CatalogItemViewModel CatalogModel { get; set; } = new CatalogItemViewModel();
+
+        public Task OnGet(CatalogItemViewModel catalogModel)
+        {
+            CatalogModel = catalogModel;
+            return Task.CompletedTask;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                await _catalogItemViewModelService.RemoveCatalogItem(CatalogModel);
+            }
+
+            return RedirectToPage("/Admin/Index");
+        }
+    }
+}
